75013314922ebae2fa7ab43b9d0250856d5a4201
dg1223 <alavi1223@hotmail.com>
2023-05-01 13:43:09 -0400

    form styling with bootstrap and react bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
@@ -7,6 +8,8 @@ import Main from "./components/Layout/Main.jsx";
 import Home from "./components/Home/Home.jsx";
 import Login from "./components/Login/Login.jsx";
 import Register from "./components/Register/Register.jsx";
+import RegisterReactBootstrap from "./components/RegisterReactBootstrap/RegisterReactBootstrap.jsx";
+import RegisterBootstrap from "./components/RegisterBoostrap/RegisterBootstrap.jsx";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +27,14 @@ const router = createBrowserRouter([
       {
         path: "/register",
         element: <Register></Register>,
+      },
+      {
+        path: "/register-rbs",
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>,
+      },
+      {
+        path: "/register-bs",
+        element: <RegisterBootstrap></RegisterBootstrap>,
       },
     ],
   },
