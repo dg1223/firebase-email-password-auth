8c53f7b06602c52c59ec40dbaa4c12b4dc59d321
dg1223 <alavi1223@hotmail.com>
2023-05-01 12:38:44 -0400

    getting email password values

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+
+  const handleEmailChange = (event) => {
+    // console.log(event.target.value);
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    // console.log(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+  };
+
   return (
     <div>
       <h4>Please register</h4>
-      <form>
-        <input type="email" name="email" id="email" placeholder="Your Enmail" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Your Email"
+        />
         <br />
         <input
+          onBlur={handlePasswordBlur}
           type="password"
           name="passowrd"
           id="password"
